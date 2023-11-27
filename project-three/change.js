b556b7851f05b20b25f353ec2d1a16674b98b627
SamJones2033 <142362316+SamJones2033@users.noreply.github.com>
2023-11-26 18:05:47 -0600

    Add files via upload

---
+++ project-three/change.js
@@ -0,0 +1,19 @@
+function change01() {
+    document.getElementById("large").src = "images/deals.jpg"
+}
+
+function change02() {
+    document.getElementById("large").src = "images/skyline.jpg"
+}
+
+function change03() {
+    document.getElementById("large").src = "images/instore.jpg"
+}
+
+function change04() {
+    document.getElementById("large").src = "images/cymbals.jpg"
+}
+
+function change05() {
+    document.getElementById("large").src = "images/sets.jpg"
+}
